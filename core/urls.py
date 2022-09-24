a48a48652742a34f1502228a7e57b30b304f9310
Mahmood Choopani <choopani.m81@gmail.com>
2022-09-24 21:09:05 +0330

    add endpoint and view for products

--- core/urls.py
+++ core/urls.py
@@ -7,13 +7,14 @@ from django.contrib import admin
 from django.urls import path, include  # add this
 
 urlpatterns = [
-    path('admin/', admin.site.urls),                     # Django admin route
-    path("",       include("apps.authentication.urls")), # Auth routes - login / register
+    path('admin/',     admin.site.urls),  # Django admin route
+    path("",           include("apps.authentication.urls")),  # Auth routes - login / register
 
     # ADD NEW Routes HERE
-    path("api/",   include("apps.api.urls")),            # API Generator Routes
-    path('',       include('apps.dyn_datatables.urls')), # Dynamic DB Routes
+    path("api/",       include("apps.api.urls")),            # API Generator Routes
+    path('',           include('apps.dyn_datatables.urls')),  # Dynamic DB Routes
+    path('ecommerce/', include('apps.ecommerce.urls')),  # E-Commerce Routes
 
     # Leave `Home.Urls` as last the last line
-    path("", include("apps.home.urls"))                  # Generic Routing
+    path("",           include("apps.home.urls"))  # Generic Routing
 ]
